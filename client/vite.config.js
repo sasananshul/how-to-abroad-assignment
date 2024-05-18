e7ebcc4e79ff2ded22533c08a05437ae613e66da
sasananshul <sasananshul@outlook.com>
2024-05-18 14:18:02 +0530

    add: dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/how-to-abroad-assignment',
+  base: '/how-to-abroad-assignment/',
   server: {
     proxy: {
       '/api': {
